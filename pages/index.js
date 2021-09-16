0c0c30eec3001093e04909545dca4598c24b0612
Brandon Krull <bk@angel.co>
2021-09-15 20:35:56 -0700

    connect button in toolbar, render rooms properly

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,47 @@
 import * as Web3 from 'web3';
 import {
+    AppBar,
     Box,
     Button,
+    Chip,
     Container,
     Grid,
     Input,
+    InputLabel,
     Link,
     MenuItem,
     Select,
     Toolbar,
     Typography,
 } from '@mui/material';
+import { alpha, makeStyles } from '@material-ui/styles';
 import { useEffect, useState } from 'react';
 
+const useStyles = makeStyles((theme) => ({
+    accountButton: {
+        position: 'relative',
+        marginLeft: 0,
+        width: '100%',
+    },
+    accountChip: {
+        width: '100px',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    toolbar: {
+        flexGrow: 1,
+    },
+}));
+
 export default function Home() {
+  const classes = useStyles();
+
   const [web3, setWeb3] = useState(null);
   const [address, setAddress] = useState(null);
+  const [activeCollection, setActiveCollection] = useState();
   const [assets, setAssets] = useState(null);
-  const [collections, setCollections] = useState(null);
+  const [collections, setCollections] = useState([]);
 
   // Get accounts and set address in state
   async function handleOnClick(_web3) {
@@ -27,37 +51,60 @@ export default function Home() {
 
         const assets = getAssets(accounts[0]);
         setAssets(assets);
-
-        console.log(assets);
     } catch (err) {
         console.error('Something went wrong:', err);
     }
   }
 
   async function getAssets(accountId) {
-      const url = `https://api.opensea.io/api/v1/assets?owner=${accountId}&order_direction=desc&offset=0&limit=20`;
+      const url = `https://api.opensea.io/api/v1/assets?owner=${accountId}&order_direction=desc&offset=0&limit=50`;
       const nfts = [];
+      const existingCollections = {};
 
       fetch(url)
           .then(resp => (resp.json()))
           .then(data => {
               var assets = data["assets"];
-              for (const asset of assets){
+              assets.map(asset => {
                   console.log(asset);
                   var id = (asset["id"]);
                   var name = (asset["name"]);
                   var permalink = (asset["permalink"]);
-                  var image_url = (asset["image_preview_url"]);
-                  var collection_name = (asset["collection"]["name"]);
-                  var collection_external_link = (asset["collection"]["external_link"]);
-                  var asset_contract_address = (asset["asset_contract"]["address"]);
-                  nfts[id]={};
-                  nfts[id]["id"]=id;
-                  nfts[id]["name"]=name;
-                  nfts[id]["collection"]=collection_name;
-                  nfts[id]["image_url"]=image_url;
-              }});
-      return nfts;
+                  var imageUrl = (asset["image_preview_url"]);
+                  var collectionName = (asset["collection"]["name"]);
+                  var collectionExternalLink = (asset["collection"]["external_link"]);
+                  var assetContractAddress = (asset["asset_contract"]["address"]);
+
+                  if (Object.keys(existingCollections).includes(collectionName)) {
+                      existingCollections[collectionName]['items'].push({
+                          id: id,
+                          name: name,
+                          permalink: permalink,
+                          imageUrl: imageUrl,
+                      });
+                  } else {
+                      existingCollections[collectionName] = {
+                          externalLink: collectionExternalLink,
+                          contractAddress: assetContractAddress,
+                          items: [{
+                              id: id,
+                              name: name,
+                              permalink: permalink,
+                              imageUrl: imageUrl,
+                          }],
+                      };
+                  }
+              });
+
+              Object.keys(existingCollections).sort().map(collection => {
+                  collections.push(
+                      <MenuItem value={collection} id={collection}>{collection}</MenuItem>
+                  );
+
+              });
+          });
+
+      return existingCollections;
   }
 
   // Connect to local blockchain
@@ -77,83 +124,86 @@ export default function Home() {
 
   return (
     <>
-    <Toolbar>TangoNFT 💃</Toolbar>
-    <Container>
-    <Grid container>
-        <Grid item xs={4}>
-            <Box m={4}>
-                <Typography variant='h5'>
-                    A private ephemeral message board for every NFT collection.
-                </Typography>
-                <p>Log-in with Metmask to see which rooms you have access to.</p>
-                {!address ? (
-                    <Button variant='contained' onClick={() => handleOnClick(web3)}>
-                    Connect to Metamask
-                    </Button>
-                ) : (
-                    <div>
-                    <Typography sx={{ display: 'flex' }}>
-                        <strong style={{ paddingRight: '.5em' }}>Welcome:</strong>{' '}
-                        {address}
+        <div className={classes.toolbar}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography variant='h5'>TangoNFT 💃</Typography>
+                    <div className={classes.accountButton}>
+                        {address ? (
+                            <Chip className={classes.accountChip} color='secondary' label={address}/>
+                        ) : (
+                            <Button color='secondary' variant='contained' onClick={() => handleOnClick(web3)}>
+                                Connect to Metamask
+                            </Button>
+                        )}
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+
+    <Container className={classes.toolbar}>
+        <Grid container>
+            <Grid item xs={6}>
+                <Box m={4}>
+                    <Typography variant='h4'>
+                        A private ephemeral message board for every NFT collection.
                     </Typography>
-                    </div>
-                )}
-            {address ? (
-              <>
-              <div className="row" id="welcome">
-                <Typography variant='h5'>Your Rooms:</Typography>
-                <small>You have access to rooms based on the NFTs you hold in your wallet.</small>
-              </div>
+                </Box>
+                <Box m={4} mt={2}>
+                    <div className="row" id="welcome">
+                        <Typography variant='h5'>Your Rooms:</Typography>
+                        {address ? (
+                        <>
+                            <small>You have access to rooms based on the NFTs you hold in your wallet.</small>
 
-              <Select
-                value=''
-                onChange={val => selectCollection(val)}
-                displayEmpty>
-                    <MenuItem value="">
-                    <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-              </>
-            ) : ''}
-            </Box>
-            <Box>
-            <p><b>What is this?</b></p>
-            <p>A little side-project by <b><Link href="https://twitter.com/pixelbeastsnft" target="_blank">PixelBeasts</Link></b>. Let us know what you think in our <Link href="https://discord.gg/YSqMfAnqzX">Discord</Link>!</p>
-            <p>There's a private message board for every NFT collection, only displaying the last 100 messages. You can only read and write in the message boards for NFT collections in which you're an owner.</p>
-            <p><i>Only works in Browser, with Metamask installed.</i></p>
-            <p>Check out <Link href="https://tangonft.com/draw/" target="_blank">Tango Draw: a drawing board for every NFT collection</Link>!</p>
-            <div className="alert alert-info">If the "submit" Button doesn't work the first time, try again. It's sometimes buggy.</div>
-            </Box>
-        </Grid>
-
-        {address && true ? (
-        <Grid container item xs={8}>
-            <Box m={4}>
-                <Grid item xs={9}>
-                    <div className="row" id="room_title"></div>
-                    <div className="row mt-4" id="messagebox">
-                    <div className="col-3">
-                        <select className="form-control" id="pfp_options"></select>
+                            <InputLabel id="collectionLabel">Collections</InputLabel>
+                            <Select
+                                value={activeCollection}
+                                onChange={event => setActiveCollection(event.target.value)}>
+                                {collections}
+                            </Select>
+                        </>
+                        ) :
+                        <small>Please connect your wallet</small>
+                        }
                     </div>
-                    <div className="col-7">
-                        <div className="form-group">
-                        <Input className="form-control" id="message_form_box"/>
+                </Box>
+
+                <Box m={4}>
+                    <p><b>What is this?</b></p>
+                    <p>A little side-project by <b><Link href="https://twitter.com/pixelbeastsnft" target="_blank">PixelBeasts</Link></b>. Let us know what you think in our <Link href="https://discord.gg/YSqMfAnqzX">Discord</Link>!</p>
+                    <p>There's a private message board for every NFT collection, only displaying the last 100 messages. You can only read and write in the message boards for NFT collections in which you're an owner.</p>
+                    <p><i>Only works in Browser, with Metamask installed.</i></p>
+                    <p>Check out <Link href="https://tangonft.com/draw/" target="_blank">Tango Draw: a drawing board for every NFT collection</Link>!</p>
+                    <div className="alert alert-info">If the "submit" Button doesn't work the first time, try again. It's sometimes buggy.</div>
+                </Box>
+            </Grid>
+
+            {address && activeCollection ? (
+            <Grid container item xs={6}>
+                <Box m={4}>
+                    <Grid item xs={9}>
+                        <div className="row" id="room_title"></div>
+                        <div className="row mt-4" id="messagebox">
+                        <div className="col-3">
+                            <select className="form-control" id="pfp_options"></select>
                         </div>
-                    </div>
-                    <div className="col-2">
-                        <Button className="btn btn-primary" id="submit" disabled>Send</Button>
-                    </div>
-                    </div>
-                    <div className="row mt-4" id="messages"></div>
-                </Grid>
-            </Box>
+                        <div className="col-7">
+                            <div className="form-group">
+                            <Input className="form-control" id="message_form_box"/>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <Button className="btn btn-primary" id="submit" disabled>Send</Button>
+                        </div>
+                        </div>
+                        <div className="row mt-4" id="messages"></div>
+                    </Grid>
+                </Box>
+            </Grid>
+            ) : ''}
         </Grid>
-        ) : ''}
-    </Grid>
-  </Container>
+    </Container>
   </>
   );
 }
